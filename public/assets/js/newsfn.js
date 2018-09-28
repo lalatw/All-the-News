949aee4f33f9ef5ffd795aed2410f7dd5362701d
L Lee <lauralee.sh@gmail.com>
2018-09-28 01:55:48 -0700

    updated notes

--- public/assets/js/newsfn.js
+++ public/assets/js/newsfn.js
@@ -65,5 +65,80 @@ $(function() {
 
   });
 
+
+
+$(document).on("click", ".noteBtn", function(event) {
+  event.preventDefault();
+  var thisId = $(this).data("id");
+  console.log("id"+thisId);
+  // Send the POST request.
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  }).then(
+    function() {
+      $("#noteList").style.visibility = "visible";
+    }
+  );  
+
+});
+
+$(document).on("click", ".noteSubmitBtn", function(event) {
+  event.preventDefault();
+  var thisId = $(this).data("id");
+  var newNote = {
+    body: $("#noteInput").val().trim(),
+  };
+  console.log("id"+thisId);
+  // Send the POST request.
+  $.ajax({
+    method: "POST",
+    url: "/api/notes" + thisId,
+    data: newNote
+  }).then(
+    function(sendData) {
+      console.log(sendData);
+      $("#noteInput").empty()
+    }
+  );
+
+});
+
+
+
+
+
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+
+
 })
 
--- server.js
+++ server.js
@@ -111,33 +111,54 @@ app.get("/api/clear", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-    db.Article.find({_id: req.params.id})
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle[0]);
-    })
-    .catch(function(err) {
-      res.json(err);
-    })
-  
-});  
-
-
-// Route for saving/updating an Article's associated Note
-
-app.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-    .then(function(dbNote){
-        return db.Article.findOneAndUpdate({_id: req.params.id}, {$set: {note: dbNote._id}}, {new: true});
-    })
-    .then(function(dbArticle) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
-    })
-    .catch(function(err) {
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
         res.json(err);
-    })
+      });
+  });
 
+  app.get("/api/articles/:id", function(req, res) {
+    db.Note.find({})
+      .then(function(dbNote) {
+        var hbsObject = {
+          notes: dbNote
+        };
+        res.render("saved", hbsObject);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
 });  
+
+
+app.post("/api/notes/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
 
 
 app.post("/api/saved/:id", function(req, res) {
@@ -153,6 +174,9 @@ app.post("/api/unsaved/:id", function(req, res) {
             console.log(dbUnSaved)
         });
 })
+
+
+
 
 // Start the server
 app.listen(PORT, function () {
