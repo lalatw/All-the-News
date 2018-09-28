be689d262ca09e82405abf3b30d827c5b5821333
L Lee <lauralee.sh@gmail.com>
2018-09-28 02:32:12 -0700

    fixed PORT setting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var request = require('request');
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -24,7 +24,7 @@ mongoose.connect(MONGODB_URI);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -120,16 +120,16 @@ app.get("/api/clear", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+   
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
+    
       .populate("note")
       .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
+        
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
+        
         res.json(err);
       });
   });
@@ -153,9 +153,7 @@ app.post("/api/notes/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
       .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+
         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
       })
       .then(function(dbArticle) {
